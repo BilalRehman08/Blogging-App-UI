7bbe8818fddd8c4fc5db1e377183d52e740ebf46
BilalRehman08 <bilalrehman080808@gmail.com>
2020-11-04 11:33:28 -0800

    fourth commit

--- lib/lib/Menu/wp-api.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
- Future<List> fetchWpPosts() async {
-   final response = await http.get('https://jsonplaceholder.typicode.com/posts', headers:{"Accept":"application/json"});
-   var convertDataJson = jsonDecode(response.body);
-   return convertDataJson;
- }
